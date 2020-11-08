f9b28f9fb00c5c3924e55e54b12cb976bb663c6a
saatvikkher <74080246+saatvikkher@users.noreply.github.com>
2020-11-08 13:28:15 +0530

    Update database.py

--- database.py
+++ database.py
@@ -14,13 +14,6 @@ cursor.execute(
 df_main = pd.read_sql_query('SELECT * FROM main', conn)
 df_user = pd.read_sql_query('SELECT * FROM user', conn)
 
-print(df_main)
-
-#from HenHealth import df_user, df_main
-
-#df_main.to_sql("main", conn, if_exists="replace")
-#df_user.to_sql("user", conn, if_exists="replace")
-
 """
 def update_main(username, password, firstname, lastname, phonenumber):
     sql = "INSERT INTO main VALUES ({},{},{},{},{})".format(username, password, firstname, lastname, phonenumber)
